53dccc16a92d9516c809ff9b085388cf1dc5b0fd
Faustine Ben Axel Ompusunggu <faustine.b.axel@banksinarmas.com>
2025-02-04 13:31:11 +0700

    update validSudoku

--- src/main/java/org/leetcode/ValidSudoku.java
+++ src/main/java/org/leetcode/ValidSudoku.java
@@ -1,5 +1,6 @@
 package org.leetcode;
 
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -9,14 +10,21 @@ public class ValidSudoku {
     }
 
     public static boolean isValidSudoku(char[][] board) {
-        Set hashSet = new LinkedHashSet();
-        for (int i = 0; i < board[0].length; i++) {
-            for (int j = 0; j < board[1].length; j++) {
-                if(hashSet.add(board[i][j])) {
-
+        HashSet<String> exist = new HashSet();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char temp = board[i][j];
+                if (board[i][j] != '.') {
+                    if (
+                            !exist.add(temp + " exist on row " + i) ||
+                                    !exist.add(temp + " exist on col " + j) ||
+                                    !exist.add(temp + " exist on box " + i / 3 + "-" + j / 3)
+                    ) {
+                        return false;
+                    }
                 }
             }
         }
-        return false;
+        return true;
     }
 }
